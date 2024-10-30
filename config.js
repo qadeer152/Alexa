1a97965f1a02ded1ac54c5e9b78c5042f1f92fa1
qadeer152 <159936975+qadeer152@users.noreply.github.com>
2024-10-30 22:35:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SY9lUb5J#ITiFI5IDbtJjLvagQcuEsyftjTzSLMkrlUzqaMpX7rs",
+SESSION_ID: process.env.SESSION_ID || "rTpQRT4b#bqE_dRpdFYTVC0fCqnGfyH2O3-6mjV9tVvZFHoyxnm8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
